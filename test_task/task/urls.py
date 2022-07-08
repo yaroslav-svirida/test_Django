5683cda0b3402cd1d20d338ca2d398f48b95c8c5
yaroslav-svirida <yaroslav.svirida@gmail.com>
2022-07-08 16:04:59 +0300

    with short_url

--- test_task/task/urls.py
+++ test_task/task/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .short_view import ShortView
 from .views import ListUsersView
 
+
 urlpatterns = [
     path('users/', ListUsersView.as_view()),
     path('short/get/', ShortView.as_view()),
